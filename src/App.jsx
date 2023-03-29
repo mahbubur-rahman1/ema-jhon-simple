2098991c66fb344b5c2341568fa359f8758bbd20
mahbubur-rahman1 <labibmrmahbub@gmail.com>
2023-03-29 22:39:50 +0600

    products

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Compunents/Header/Header'
+import Shop from './Compunents/Shop/Shop'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
      <Header></Header>
+     <Shop></Shop>
     </div>
   )
 }
